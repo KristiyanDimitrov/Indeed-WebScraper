9ba2e84309298d59bcd5514bc18627003ca55eb5
Chris9606 <dimitr14@uni.coventry.ac.uk>
2018-07-16 20:34:49 +0100

    Interpreter encoding issue solved by typing the following in the cmd: chcp 65001

--- Indeed.py
+++ Indeed.py
@@ -7,7 +7,7 @@ from lxml import etree
 import time
 
 
-def get_all_jobs(page_link):
+def get_info(page_link):
     try:
         page_response = requests.get(page_link, timeout=10)
         if page_response.status_code == 200:
@@ -26,47 +26,11 @@ def get_all_jobs(page_link):
     else:
         return None
 
-def get_job():
-    page_cont = get_all_jobs(page_link)
-    for link in page_cont.findAll('a', attrs={'class': 'jobtitle'}):
-        links = get_all_jobs(base + link['href'])
-        yield links
-
 headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
 page_link ='https://www.indeed.co.uk/jobs?q=python&l=West+Midlands'
 base = 'https://www.indeed.co.uk'
-
-jobs= get_job()
-jobs_text = {}
-index = 0
-summary = ''
-
-for job in jobs:
-
-    for item in job.findAll('span', attrs={'class': 'summary'}):
-        for element in item:
-            try:
-                summary += element.text
-                print(element.text)
-            except Exception as e:
-                print("Err passed by TRY: " + str(e))
-                print("<<<<<<<<<<<<<<<<<<")
-                print("RUN TIME ERR due to unexpected element")
-                print(">>>>>>>>>>>>>>>>>>>>>")
-                print("Type of element :" + str(type(element)))
-                if str(type(element)) == "<class 'bs4.element.Tag'>":
-                    print("Passing element:")
-                    print(element)
-                else:
-                    summary += element
-                    print(element)
-    temp = index
-    jobs_text[temp] = summary
-    index += 1
-    print("Entry recorded!")
-    break
-
-print(jobs_text[0])
-print(jobs_text[1])
-time.sleep(20000)
-
+page_cont = get_info(page_link)
+for link in page_cont.findAll('a', attrs={'class':'jobtitle'}):
+        test = get_info( base + link['href'])
+        print(test)
+        time.sleep(20000)
