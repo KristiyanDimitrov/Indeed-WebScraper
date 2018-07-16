68a6eb1f0667b4ed64040ed228a1adf84f721408
Chris9606 <dimitr14@uni.coventry.ac.uk>
2018-07-17 00:05:48 +0100

    Works, but isn't getting all of the jobs and has no order.Maybe not the full job-search page is loading?

--- Indeed.py
+++ Indeed.py
@@ -1,18 +1,14 @@
+import soap, urllib, requests
 from bs4 import BeautifulSoup as bs
-import requests
 from user_agent import generate_user_agent
-import soap
-import urllib
 from lxml import etree
-import time
 
 
-def get_info(page_link):
+def get_all_jobs(page_link):
     try:
         page_response = requests.get(page_link, timeout=10)
         if page_response.status_code == 200:
-            #page_content = BeautifulSoup(page_response.content, "html.parser")
-            page_content = bs(urllib.urlopen(page_link, "lxml"))
+            page_content = bs(urllib.urlopen(page_link, "xml"))
         else:
             print ("Failed!")
             print(page_response.status_code)
@@ -26,11 +22,57 @@ def get_info(page_link):
     else:
         return None
 
+def get_job(page_cont):
+    for link in page_cont.findAll('a', attrs={'class': 'jobtitle'}):
+        links = get_all_jobs(base + link['href'])
+        yield links
+
+def get_job_info(jobs):
+    summary = ''
+    readings = []
+    for job in jobs:
+
+        for item in job.findAll('span', attrs={'class': 'summary'}):
+            for element in item:
+                try:
+                    summary += element.text
+                    # print(element.text)
+                except Exception as e:
+                    print("Error passed by TRY: " + str(e))
+                    print("Type of element :" + str(type(element)))
+                    if str(type(element)) == "<class 'bs4.element.Tag'>":
+                        print("Passing element:")
+                        # print(element)
+                    else:
+                        summary += element
+                        print(element)
+        job_title = job.find('b', attrs={'class': 'jobtitle'})
+        if job_title is None:
+            continue
+        else:
+            job_title = job_title.text
+        read = {
+            'title': job_title,
+            'summary': summary
+        }
+        readings.append(read)
+        summary = ""
+        print("Entry recorded!")
+    return readings
+
+
+
 headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
 page_link ='https://www.indeed.co.uk/jobs?q=python&l=West+Midlands'
 base = 'https://www.indeed.co.uk'
-page_cont = get_info(page_link)
-for link in page_cont.findAll('a', attrs={'class':'jobtitle'}):
-        test = get_info( base + link['href'])
-        print(test)
-        time.sleep(20000)
+page_cont = get_all_jobs(page_link)
+
+jobs= get_job(page_cont)
+readings = get_job_info(jobs)
+
+for reading in readings:
+    print(reading)
+    print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+    print('>>>>>>>>>>>>>>>>>>>' + reading['title'] + '>>>>>>>>>>>>>>>>>')
+    print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+
