b0a3fcd1d53ea9ab70f14691714cbb8dd749be46
Chris9606 <dimitr14@uni.coventry.ac.uk>
2018-07-18 14:06:20 +0100

    All jobs in a page covered, going to next page added. Bugs: script runs twice, user error from soap printing about parser issue.

--- Indeed.py
+++ Indeed.py
@@ -2,13 +2,14 @@ import soap, urllib, requests
 from bs4 import BeautifulSoup as bs
 from user_agent import generate_user_agent
 from lxml import etree
+import time
 
 
 def get_all_jobs(page_link):
     try:
         page_response = requests.get(page_link, timeout=10)
         if page_response.status_code == 200:
-            page_content = bs(urllib.urlopen(page_link, "xml"))
+            page_content = bs(urllib.urlopen(page_link))
         else:
             print ("Failed!")
             print(page_response.status_code)
@@ -18,16 +19,23 @@ def get_all_jobs(page_link):
     except:
         print(" Unknown exception!")
     if 'page_content' in locals():
+        #next_page = page_content.find('div', attrs={'class': 'pagination'})
+        #print(next_page)
+        #time.sleep(20000)
+        #next_page = next_page.find_last('a')
         return page_content
     else:
         return None
 
 def get_job(page_cont):
-    for link in page_cont.findAll('a', attrs={'class': 'jobtitle'}):
+    jobs_1 = page_cont.findAll('a', attrs={'class': 'jobtitle'})
+    jobs_2 = page_cont.findAll('a', attrs={'class': 'turnstileLink'})
+    jobs = jobs_1 + jobs_2
+    for link in jobs :
         links = get_all_jobs(base + link['href'])
         yield links
 
-def get_job_info(jobs):
+def get_job_info(jobs, pageN):
     summary = ''
     readings = []
     for job in jobs:
@@ -38,41 +46,66 @@ def get_job_info(jobs):
                     summary += element.text
                     # print(element.text)
                 except Exception as e:
-                    print("Error passed by TRY: " + str(e))
-                    print("Type of element :" + str(type(element)))
+                    #print("Error passed by TRY: " + str(e))
+                    #print("Type of element :" + str(type(element)))
                     if str(type(element)) == "<class 'bs4.element.Tag'>":
                         print("Passing element:")
-                        # print(element)
+                        #print(element)
                     else:
                         summary += element
-                        print(element)
+                        #print(element)
         job_title = job.find('b', attrs={'class': 'jobtitle'})
         if job_title is None:
             continue
         else:
             job_title = job_title.text
         read = {
+            'page_number': pageN,
             'title': job_title,
             'summary': summary
         }
         readings.append(read)
         summary = ""
         print("Entry recorded!")
+
+    time.sleep(4)
     return readings
 
 
-
+numberOfPages_toRead = 3
 headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
 page_link ='https://www.indeed.co.uk/jobs?q=python&l=West+Midlands'
 base = 'https://www.indeed.co.uk'
-page_cont = get_all_jobs(page_link)
+next_page = 1
 
-jobs= get_job(page_cont)
-readings = get_job_info(jobs)
 
-for reading in readings:
-    print(reading)
-    print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
-    print('>>>>>>>>>>>>>>>>>>>' + reading['title'] + '>>>>>>>>>>>>>>>>>')
-    print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+while next_page < numberOfPages_toRead:
+    page_cont = get_all_jobs(page_link)
+    jobs= get_job(page_cont)
+    readings = get_job_info(jobs, next_page)
+
+    # Constructing next page link
+    page_link = ("https://www.indeed.co.uk/jobs?q=python&l=West+Midlands&start=" + str(next_page*10))
+    next_page += 1
+
+    # Print found job titles
+    for read in readings:
+        print(read['title'])
+    time.sleep(15)
+
+    # Store page to testing
+    page_name = 'page_' + str(next_page-1) +'.html'
+    with open(page_name, 'a') as the_file:
+        the_file.write(str(page_cont))
+    print("Check the file!")
+
+    print("Navigate to page: " + str(next_page))
+    print(page_link)
+
+
+#for reading in readings:
+#    print(reading)
+#    print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+#    print('>>>>>>>>>>>>>>>>>>>' + reading['title'] + '>>>>>>>>>>>>>>>>>')
+#    print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
 
